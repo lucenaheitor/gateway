1546c44196a34683b7271c38db6c71ed5bf4b779
Heitor <lucenaheitor@gmail.com>
2024-10-14 09:35:43 -0300

    gateway criado

---
+++ src/test/java/lucenaheitor/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lucenaheitor.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
